de69cd60c1a567e5fc74bd926504041cb73b3d4d
Brahmajit Das <brahmajit.xyz@gmail.com>
2024-08-28 20:39:43 +0530

    Enable features for qmk-vim
    - VIM_G_MOTIONS
    - VIM_PASTE_BEFORE
    - VIM_REPLACE
    - VIM_W_BEGINNING_OF_WORD
    - VIM_NUMBERED_JUMPS
    
    Signed-off-by: Brahmajit Das <brahmajit.xyz@gmail.com>

--- keyboards/keychron/k6/rgb/config.h
+++ keyboards/keychron/k6/rgb/config.h
@@ -59,6 +59,11 @@
 #define BETTER_VISUAL_MODE
 #define VIM_I_TEXT_OBJECTS
 #define VIM_A_TEXT_OBJECTS
+#define VIM_G_MOTIONS
+#define VIM_PASTE_BEFORE
+#define VIM_REPLACE
+#define VIM_W_BEGINNING_OF_WORD
+#define VIM_NUMBERED_JUMPS
 #endif
 
 /* RGB Matrix Effects */
