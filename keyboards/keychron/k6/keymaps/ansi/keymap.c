401aab02bfb67b4999eb02471ef3cd5e3db4bb2e
Brahmajit Das <brahmajit.xyz@gmail.com>
2024-08-28 20:40:45 +0530

    Remove unused global variable
    Signed-off-by: Brahmajit Das <brahmajit.xyz@gmail.com>

--- keyboards/keychron/k6/keymaps/ansi/keymap.c
+++ keyboards/keychron/k6/keymaps/ansi/keymap.c
@@ -22,7 +22,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifdef USE_VIM
 #include "qmk-vim/src/vim.h"
 #include "qmk-vim/src/modes.h"
-int vim_mode = 0;
 #endif
 
 #include QMK_KEYBOARD_H
